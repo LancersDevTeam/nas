9956303ba808142b777121ea129e3c761b433eef
odrum428 <oxkeita@gmail.com>
2020-04-18 23:11:49 +0900

    add load_send_nas_num method

---
+++ src/db.py
@@ -0,0 +1,24 @@
+import os
+
+import boto3
+from boto3.dynamodb.conditions import Key
+from utils import get_ref_timestamp
+
+# Reading the lambda environment variable
+NAS_SEND_LIMIT = int(os.envirion["TIP_LIMIT"])
+
+
+def load_send_nas_num(user_id):
+    ref_timestamp = get_ref_timestamp()
+    try:
+        dynamoDB = boto3.resource('dynamodb')
+        table = dynamoDB.Table('NAS')
+
+        send_nas_data_this_week = table.query(
+            KeyConditionExpression=Key('tip_user_id').eq(user_id) & Key('time_stamp').gt(ref_timestamp)
+        )
+        send_nas_num = send_nas_data_this_week['Count']
+        return send_nas_num
+    except Exception as e:
+        print(e)
+        return 0
