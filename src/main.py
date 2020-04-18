5cc615c308d35cbd5fb7f672bebad0fede656299
odrum428 <oxkeita@gmail.com>
2020-04-18 21:39:11 +0900

    upload responce code

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import threading
+import requests
 
 
 def lambda_handler(event, content):
@@ -9,13 +10,13 @@ def lambda_handler(event, content):
 # If the response is slow, Slack will throw an error, so return 200 codes instantly.
 def return_200():
     print("return 200")
-    return 200
+    return requests.codes.ok
 
 def main_func(event, content):
     # Ignore the retry process from Slack
     if 'X-Slack-Retry-Num' in event['params']['header']:
         print("this is redirect requests!")
-        return 200
+        return 204
     
     # write a handler for each process below.
-    return 200
+    return requests.codes.ok
