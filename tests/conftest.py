ab07a6927d82b2a05de04a90c7986e771b366e93
odrum428 <oxkeita@gmail.com>
2020-04-28 15:07:27 +0900

    add nas_gacha_db fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,3 +40,40 @@ def nas_db():
 
         nas_db = dynamoDB.Table('NAS')
         yield nas_db
+
+
+@pytest.fixture
+def nas_gacha_db():
+    with mock_dynamodb2():
+        os.environ['AWS_DEFAULT_REGION'] = 'ap-northeast-1'
+        dynamoDB = boto3.resource('dynamodb')
+        dynamoDB.create_table(
+            TableName='NAS_GACHA',
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'user_id',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'time_stamp',
+                    'AttributeType': 'N'
+                }
+            ],
+            KeySchema=[
+                {
+                    'AttributeName': 'user_id',
+                    'KeyType': 'HASH'
+                },
+                {
+                    'AttributeName': 'time_stamp',
+                    'KeyType': 'RANGE'
+                },
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5,
+            }
+        )
+
+        nas_gacha_db = dynamoDB.Table('NAS_GACHA')
+        yield nas_gacha_db
