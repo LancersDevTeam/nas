d26b23fb552757f244bfd86ddcfc83ba4ff2a980
odrum428 <oxkeita@gmail.com>
2020-04-30 03:14:07 +0900

    add bring_slack_id_from_slack_name func

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
+import os
 from datetime import datetime, timedelta
 from decimal import Decimal
 from .db import scan_nas_records
 from collections import Counter
+import requests
 
 import pytz
 
@@ -52,3 +54,21 @@ def parse_lambda_event_str(lambda_event_obj):
         content_value = param[content_separate_index+1:]
         parsed_dict[content_key] = content_value
     return parsed_dict
+
+
+def bring_slack_id_from_slack_name(user_name):
+    try:
+        slack_api_url = "https://slack.com/api/users.list"
+        slack_oauth_token = os.environ["SLACK_OAUTH_ACCESS_TOKEN"]
+
+        params = {'token': slack_oauth_token}
+        user_list = requests.get(slack_api_url, params=params)
+        print(user_list)
+        for user in user_list["members"]:
+            if user_name == user["name"]:
+                user_id = user['id']
+                break
+        return user_id
+    except Exception as e:
+        print(e)
+        return ''
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,8 +3,9 @@ import sys
 import time
 from datetime import datetime
 from decimal import Decimal
+import json
 
-from src.utils import get_last_week_ref_timestamp, get_ref_timestamp, calc_nas_ranking_this_week
+from src.utils import get_last_week_ref_timestamp, get_ref_timestamp, calc_nas_ranking_this_week, bring_slack_id_from_slack_name
 
 sys.path.append('../')
 
@@ -92,3 +93,20 @@ def test_calc_nas_ranking_this_week(nas_db):
         'test_user_C_name': 2
     }
     assert calc_nas_ranking_this_week() == estimate_nas_ranking
+
+
+def test_bring_slack_id_from_slack_name(mocker):
+    """Get the slack_user_name from the slack_user_id
+    There are cases in which it is not possible to get enough information about the destination party, etc., in running the NAS.
+    In that case, use the slack api to get the user_name directly from the group's user list.
+
+    Return:
+        str: slack user name
+    """
+    # Create fake response
+    responseMock = mocker.Mock()
+    responseMock.status_code = 200
+    responseMock.text = json.dumps({'test_user_B_id': 'test_user_B_name'})
+
+    mocker.patch('requests.get').return_value = responseMock
+    assert bring_slack_id_from_slack_name('test_user_Z_id') == ''
